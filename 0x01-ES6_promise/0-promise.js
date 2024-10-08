2b283f55b92404ae44579afad9413f9eac4af9ae
jeremymordecai628 <jeremymordecai08@gmail.com>
2024-10-08 22:06:35 +0300

    task0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,1 +1,7 @@
-0-promise.js0-promise.js
+export default function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+      // You can resolve or reject the promise based on your conditions.
+      resolve('API response received');
+    });
+  }
+  
